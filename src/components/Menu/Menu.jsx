bc2ca2adcfe8062b498bbe5c6da35877d1e005f3
찬민 <a1@chanmin-ui-MacBookAir.local>
2021-09-29 22:32:50 +0900

    fix: 페이지 이동 로직 수정

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,25 +2,30 @@ import React, { useEffect, useState } from "react";
 import BaseCard from "components/common/BaseCard/BaseCard";
 import MenuItem from "./MenuItem";
 import { useRecoilState } from "recoil";
-import { languageState } from "store/store";
+import { languageState, studyModeState } from "store/store";
 import language from "constants/language";
 import MenuType from "./MenuType";
 
 const Menu = () => {
-  const [selectedLanguage, setSelectedLanguage] = useRecoilState(languageState);
-  useEffect(() => { }, [selectedLanguage]);
+  const [, setSelectedLanguage] = useRecoilState(languageState);
+  const [, setSelectedStudyMode] = useRecoilState(studyModeState)
+
+  const handleSelectLanguage = (language) => {
+    setSelectedLanguage(language)
+    setSelectedStudyMode(null)
+  }
 
   return (
     <BaseCard>
       <ul className="">
         <MenuItem
           title="한국어 (TOPIK)"
-          onClick={() => setSelectedLanguage(language.KOREAN)}
+          onClick={() => handleSelectLanguage(language.KOREAN)}
           language={language.KOREAN}
         />
         <MenuItem
           title="영어 (OPIC)"
-          onClick={() => setSelectedLanguage(language.ENGLISH)}
+          onClick={() => handleSelectLanguage(language.ENGLISH)}
           language={language.ENGLISH}
         />
       </ul>
--- src/components/TestList/TestList.jsx
+++ src/components/TestList/TestList.jsx
@@ -1,18 +1,30 @@
 import BaseCard from 'components/common/BaseCard/BaseCard'
 import BaseSubtitle from 'components/common/BaseSubtitle/BaseSubtitle'
 import TestItem from 'components/TestListItem/TestListItem'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const TestList = () => {
+  const [testList, setTestList] = useState([])
+
+  const fetchTestList = async () => {
+    const testList = await import("data/koreanTest.json").then(res => console.log(res))
+    console.log(testList)
+    setTestList(testList)
+  }
+
+  useEffect(() => {
+    fetchTestList()
+  }, [])
+
   return (
     <>
       <BaseSubtitle text="예문 목록" />
       <BaseCard className="block">
         <ul className="block">
-          <TestItem id="1" text="아버지 가방에 들어가신다" difficulty="쉬움" />
-          <TestItem id="2" text="아버지 가방에 들어가신다" difficulty="쉬움" />
-          <TestItem id="3" text="아버지 가방에 들어가신다" difficulty="쉬움" />
-          <TestItem id="4" text="아버지 가방에 들어가신다" difficulty="쉬움" />
+          {testList?.map(testData => {
+            const { id, text, difficulty, language } = testData
+            return <TestItem id={id} text={text} difficulty={difficulty} />
+          })}
         </ul>
       </BaseCard>
     </>
