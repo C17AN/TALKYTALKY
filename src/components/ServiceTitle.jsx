a1299fce2ea6e146152dd4ffa71c4ed0fc47add9
찬민 <a1@chanmin-ui-MacBookAir.local>
2021-10-01 18:28:35 +0900

    fix: 타이틀 스타일 복구, 영문 데이터 추가

--- src/components/ServiceTitle.jsx
+++ src/components/ServiceTitle.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const ServiceTitle = () => {
   return (
     <BaseCard className="mb-4">
-      <header className="inline-block p-4">
+      <header className="inline-block p-4 w-full">
         <h1 className="font-montserrat font-bold text-center text-xl">
           <a href='/'>TALKY<span className="text-blue-600">TALKY</span></a>
         </h1>
--- src/components/TestListItem/TestListItem.jsx
+++ src/components/TestListItem/TestListItem.jsx
@@ -4,7 +4,7 @@ import TestListItemButton from './TestListItemButton'
 const TestListItem = ({ id, text, difficulty }) => {
   return (
     <li className="flex items-center justify-between my-4 mx-4 p-2 pl-4 border border-gray-100 hover:border-blue-400 transition-colors shadow-sm rounded-md">
-      <p>{text}</p>
+      <div className="flex"><p className="mr-2">{id}.</p><p>{text}</p></div>
       <section className="flex space-x-4">
         <p className="bg-blue-100 rounded-md p-1 px-2 text-sm">{difficulty}</p>
         <TestListItemButton id={id} text="풀어보기" />
