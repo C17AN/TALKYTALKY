1b6da6578b6185e00e4ce755e73fea1355e93778
찬민 <a1@chanmin-ui-MacBookAir.local>
2021-10-02 23:39:29 +0900

    feat: implement recorder component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="absolute flex items-center justify-between bottom-0 w-screen h-16 bg-blue-100 px-8">
-      <p>토키<span className="text-blue-500">토키</span> @2021</p>
-      <p className="text-gray-400 text-sm">본 서비스는 ETRI(한국전자통신연구원)와 KAKAO 음성 합성 API를 활용합니다.</p>
+      <p>TEAM TALKY<span className="text-blue-500">TALKY</span> @2021</p>
+      <p className="text-gray-400 text-sm">본 서비스는 ETRI(한국전자통신연구원) 발음평가 API와 GOOGLE 음성 합성 API를 활용합니다.</p>
     </footer>
   )
 }
