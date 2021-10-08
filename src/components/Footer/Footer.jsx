1a03cceaa0a452ebb61845389a7aed6cd284f57d
c17an <kimchanmin1@gmail.com>
2021-10-08 11:53:48 +0900

    style: 푸터 텍스트 호버 효과 추가

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,8 +4,17 @@ const Footer = () => {
   return (
     <footer className="absolute flex items-center justify-between bottom-0 w-screen h-16 bg-blue-100 px-8">
       <p>TEAM TALKY<span className="text-blue-500">TALKY</span> @2021</p>
-      <p className="text-gray-400 text-sm">본 서비스는 ETRI(한국전자통신연구원) 발음평가 API와 GOOGLE 음성 합성 API를 활용합니다.</p>
-    </footer>
+      <div className="text-gray-400 text-sm">본 서비스는 <a href="https://aiopen.etri.re.kr/guide_pronunciation.php"
+        target="_blank"
+        rel="noreferrer"
+        className="text-blue-400 hover:underline"
+      >ETRI(한국전자통신연구원) 발음평가 API</a>와 <a href="https://cloud.google.com/text-to-speech?hl=ko"
+        target="_blank"
+        rel="noreferrer"
+        className="text-blue-400 hover:underline">
+          GOOGLE 음성 합성 API를 활용합니다.
+        </a></div>
+    </footer >
   )
 }
 
--- src/utils/parseCategoryName.ts
+++ src/utils/parseCategoryName.ts
@@ -3,7 +3,7 @@ import CATEGORY from "constants/category";
 export const parseCategoryName = (category) => {
   switch (category) {
     case CATEGORY.ART:
-      return "예술";
+      return "문학";
     case CATEGORY.TRAVEL:
       return "여행";
     case CATEGORY.DAILY:
