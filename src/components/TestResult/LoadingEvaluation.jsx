fcf0dc13b057026703542a170db15cc064818d55
c17an <kimchanmin1@gmail.com>
2021-10-12 22:45:50 +0900

    chore: 대기 텍스트 추가

--- src/components/TestResult/LoadingEvaluation.jsx
+++ src/components/TestResult/LoadingEvaluation.jsx
@@ -6,7 +6,10 @@ const LoadingEvaluation = () => {
   return (
     <div className="flex items-center justify-center">
       <img src={loadingAnimation} alt="loading" className="loading-svg" />
-      <p className="font-semibold text-lg ml-2 text-gray-500">평가를 기다리고 있습니다.</p>
+      <div>
+        <p className="font-semibold text-lg ml-2 text-gray-500">평가를 기다리고 있습니다.</p>
+        <p className="ml-2 text-gray-400 text-sm">(녹음 완료 후, 약 1 ~ 3초의 시간이 소요됩니다.)</p>
+      </div>
     </div>
   )
 }
