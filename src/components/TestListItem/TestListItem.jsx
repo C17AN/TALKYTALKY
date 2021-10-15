0f2f0d03757123e39169ee0533eac099df208d63
c17an <kimchanmin1@gmail.com>
2021-10-15 23:59:14 +0900

    fix: 메뉴 진입 사용성 개선

--- src/components/TestListItem/TestListItem.jsx
+++ src/components/TestListItem/TestListItem.jsx
@@ -2,30 +2,40 @@ import { motion } from 'framer-motion'
 import TestListItemButton from './TestListItemButton'
 import "./TestListItem.scss"
 import { categoryClassNameSetter, difficultyClassNameSetter } from 'utils/classNameSetter'
+import { Link } from "react-router-dom"
 import { parseCategoryName } from 'utils/parseCategoryName'
 import { parseDifficultyName } from 'utils/parseDifficultyName'
+import { useRecoilState } from 'recoil'
+import { languageState, studyModeState } from 'store/store'
 
 const TestListItem = ({ id, text, category, difficulty }) => {
+
+  const [language] = useRecoilState(languageState)
+  const [studyMode] = useRecoilState(studyModeState)
+
   return (
-    <motion.li
-      transition={{
-        duration: 0.2
-      }}
-      whileHover={{
-        scale: 1.005,
-      }
-      }
-      className="flex items-center justify-between my-4 mx-4 p-2 pl-4 border border-gray-100 hover:bg-blue-50 transition-colors shadow-sm rounded-md cursor-pointer" >
-      <div className="flex text-list-text-container">
-        <p className="pr-2">{id}.</p>
-        <p className="pl-1 test-list-text">{text}</p>
-      </div>
-      <section className="flex space-x-4">
-        <p className={`${categoryClassNameSetter(category)} rounded-md p-1 px-2 text-sm`}>{parseCategoryName(category)}</p>
-        <p className={`${difficultyClassNameSetter(difficulty)} rounded-md p-1 px-2 text-sm`}>{parseDifficultyName(difficulty)}</p>
-        <TestListItemButton id={id} text="풀어보기" />
-      </section>
-    </motion.li >
+    <Link to={`/${studyMode}/${language}/${id}`}>
+      <motion.li
+        transition={{
+          duration: 0.2
+        }}
+        whileHover={{
+          scale: 1.005,
+        }
+        }
+        className="flex items-center justify-between my-4 mx-4 p-2 pl-4 border border-gray-100 hover:bg-blue-50 transition-colors shadow-sm rounded-md cursor-pointer" >
+
+        <div className="flex text-list-text-container">
+          <p className="pr-2">{id}.</p>
+          <p className="pl-1 test-list-text">{text}</p>
+        </div>
+        <section className="flex space-x-4">
+          <p className={`${categoryClassNameSetter(category)} rounded-md p-1 px-2 text-sm`}>{parseCategoryName(category)}</p>
+          <p className={`${difficultyClassNameSetter(difficulty)} rounded-md p-1 px-2 text-sm`}>{parseDifficultyName(difficulty)}</p>
+          <TestListItemButton id={id} text="풀어보기" />
+        </section>
+      </motion.li >
+    </Link>
   )
 }
 
--- src/components/TestListItem/TestListItemButton.jsx
+++ src/components/TestListItem/TestListItemButton.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { useRecoilState } from 'recoil'
-import { languageState, studyModeState } from 'store/store'
 
 const TestListItemButton = ({ text, onClick, id }) => {
-  const [language] = useRecoilState(languageState)
-  const [studyMode] = useRecoilState(studyModeState)
-
   return (
-    <Link to={`/${studyMode}/${language}/${id}`}>
       <button onClick={onClick} className="p-1 px-3 bg-indigo-50 rounded-md text-sm">
         {text}
       </button>
-    </Link>
   )
 }
 
