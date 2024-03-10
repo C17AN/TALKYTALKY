cba92a8dee144374309100905299d62abd9e21b7
chanmin <kimchanmin1@gmail.com>
2024-03-10 15:34:32 +0900

    Revert "Delete src/App.js"
    This reverts commit 14405cb20b2a08591964490cef29d2b457499386.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import MainLayout from "./layout/MainLayout/MainLayout";
+
+function App() {
+  return (
+    <div className="App">
+      <MainLayout />
+    </div>
+  );
+}
+
+export default App;
